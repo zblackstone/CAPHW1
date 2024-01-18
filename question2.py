c8236b9d7d9725a0b80d8a7b3617dd6e593c33b1
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-01-18 10:25:04 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,15 +1,19 @@
 def question2_1():
-    # answer = ['string', 'string', 'string']
+    answer = ['Discrete', 'Quantitative', 'Ratio']
     return answer
 
 def question2_2():
+    answer = ['Continuous', 'Quantitative', 'Ratio']
     return answer
 
 def question2_3():
+    answer = ['Continuous', 'Quantitative', 'Ordinal']
     return answer 
 
 def question2_4():
+    answer = ['Discrete', 'Qualitative', 'Normal']
     return answer
 
 def question2_5():
+    answer = ['Discrete', 'Qualitative', 'Nominal']
     return answer
