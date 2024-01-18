77c063eecd13a3916d6cca9b009109ffde188925
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-01-18 10:45:19 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,16 +1,24 @@
 def question5_1():
-    """
+    
     answer = {
-            'bin1': [d1,d2,d3...]
-            'bin2': [e1,...],
-            'bin3': [f1,...]
-    }
-    where di, ei, fi are integers. 
-    """
+            'bin1': [1, 2, 3, 4, 5],
+            'bin2': [6, 7, 8],
+            'bin3': [9]
+    }       
     return answer
 
 def question5_2():
+    answer = {
+            'bin1': [1, 2, 3],
+            'bin2': [4, 5, 6],
+            'bin3': [7, 8, 9]
+    }
     return answer
 
 def question5_3():
+    answer = {
+        'bin1': [1],
+        'bin2': [2, 3, 4]
+        'bin3': [5, 6, 7, 8, 9]
+    }
     return answer 
