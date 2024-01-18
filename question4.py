b249622059d06b19a8a55a4df34a48e157fd887c
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-01-18 10:33:42 -0500

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,24 +1,31 @@
 def question4_1():
-    # answer = ['string', 'string']
+    answer = ['Nominal', 'Ordinal']
     return answer
 
 def question4_2():
+    answer = ['Interval', 'Ordinal']
     return answer
 
 def question4_3():
+    answer = ['Ratio', 'Ordinal']
     return answer 
 
 def question4_4():
+    answer = ['Ratio', 'Ordinal']
     return answer
 
 def question4_5():
+    answer = ['Ratio', 'Ratio']
     return answer
 
 def question4_6():
+    answer = ['Ratio', 'Ordinal']
     return answer
 
 def question4_7():
+    answer = ['Ratio', 'Ratio']
     return answer
 
 def question4_8():
+    answer = ['Ratio', 'Interval']
     return answer
