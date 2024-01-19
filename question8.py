6aae3993b9df106e6421cf00e160235651d52ab5
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-01-19 00:57:22 -0500

    Update question8.py

--- question8.py
+++ question8.py
@@ -1,14 +1,15 @@
 def question8_1():
-    """
-    answer= 'string'  or ['pt1', 'pt2']
-    """
+    answer= ['x1', 'x2']
     return answer
 
 def question8_2():
+    answer = ['x1', 'x2']
     return answer
 
 def question8_3():
+    answer = 'equally similar'
     return answer 
 
 def question8_4():
+    answer = 'equally similar'
     return answer 
