6ad4808c9610ee5e20ce7f63fc55133bc43baf9f
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-01-19 00:58:53 -0500

    Update question9.py

--- question9.py
+++ question9.py
@@ -1,18 +1,22 @@
 def question9_1():
     """
-    answer = string'
+    answer = 'SMC'
     """
     return answer
 
 def question9_2():
+    answer = 'Jaccard'
     return answer
 
 def question9_3():
+    answer = 'Euclidean'
     return answer 
 
 def question9_4():
+    ansewr = 'Euclidean'
     return answer 
 
 def question9_5():
+    answer = 'Cosine Similarity'
     return answer 
 
