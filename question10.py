1dc31deecfbcbed3fb3c8a853ef4bca577ccdd3f
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-01-19 01:01:03 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,32 +1,39 @@
 def question10_1():
-    """
-    answer: python bool (True or False)
-    """
+    answer = False
     return answer
 
 def question10_2():
+    answer = True
     return answer
 
 def question10_3():
+    answer = False
     return answer
 
 def question10_4():
+    answer = True
     return answer
 
 def question10_5():
+    answer = False
     return answer
 
 def question10_6():
+    answer = False
     return answer
 
 def question10_7():
+    answer = True
     return answer
 
 def question10_8():
+    answer = False
     return answer
 
 def question10_9():
+    answer = False
     return answer
 
 def question10_10():
+    answer = False
     return answer
