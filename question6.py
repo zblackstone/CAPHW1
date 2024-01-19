59b58141f579b0ed15514c50e6ac76ebf07aed7e
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-01-19 16:43:50 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,21 +1,21 @@
 def question6_1():
     answer = {
-        'equal_width': ['Change', 3],
-        'equal_frequency': ['No Change', 9]
+        'equal_width': ['change', 3],
+        'equal_frequency': ['no change', 9]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['Change', 2],
-        'equal_frequency': ['No change', 9]
+        'equal_width': ['change', 2],
+        'equal_frequency': ['no change', 9]
     }
     return answer
 
 def question6_3():
     answer = {
-        'equal_width': ['Change', 2],
-        'equal_frequency': ['Change', 2]
+        'equal_width': ['change', 2],
+        'equal_frequency': ['change', 2]
     }
     return answer 
 
